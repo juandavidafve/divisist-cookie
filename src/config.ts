ab3101fc84cca344578d91ddd5f9f9e843eede80
Juan Afanador <juanafanador07@gmail.com>
2025-11-02 22:30:39 -0500

    fix: 2 factor Auth

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,16 @@ const envSchema = z.object({
   PORT: z.coerce.number().int().positive().default(3000),
   GOOGLE_EMAIL: z.email(),
   GOOGLE_PASSWORD: z.string().min(1),
+  GOOGLE_TOTP_SECRET: z.string().min(1),
   CHROMIUM_EXECUTABLE: z.string().default("/usr/bin/chromium"),
 });
 
 const env = envSchema.parse(process.env);
 
-export const { PORT, GOOGLE_EMAIL, GOOGLE_PASSWORD, CHROMIUM_EXECUTABLE } = env;
+export const {
+  PORT,
+  GOOGLE_EMAIL,
+  GOOGLE_PASSWORD,
+  GOOGLE_TOTP_SECRET,
+  CHROMIUM_EXECUTABLE,
+} = env;
--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,8 +1,14 @@
-import { Browser } from "puppeteer-core";
+import { authenticator } from "otplib";
+import { Browser, Page } from "puppeteer-core";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
-import { CHROMIUM_EXECUTABLE, GOOGLE_EMAIL, GOOGLE_PASSWORD } from "./config";
+import {
+  CHROMIUM_EXECUTABLE,
+  GOOGLE_EMAIL,
+  GOOGLE_PASSWORD,
+  GOOGLE_TOTP_SECRET,
+} from "./config";
 
 const stealth = StealthPlugin();
 stealth.enabledEvasions.delete("iframe.contentWindow");
@@ -21,7 +27,9 @@ export async function getCookie() {
     "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36",
   );
 
-  await page.goto("https://divisist2.ufps.edu.co/");
+  await page.goto("https://divisist2.ufps.edu.co/", {
+    waitUntil: "domcontentloaded",
+  });
 
   await page.evaluate(() => {
     // @ts-ignore
@@ -29,9 +37,7 @@ export async function getCookie() {
   });
 
   await page.waitForNavigation();
-
   await page.locator("input[type=email]").fill(GOOGLE_EMAIL);
-
   await page.keyboard.press("Enter");
 
   const passwordInput = await page.waitForSelector("input[type=password]", {
@@ -42,9 +48,16 @@ export async function getCookie() {
 
   await page.keyboard.press("Enter");
 
-  await page.waitForSelector(".user-image", {
-    timeout: 5000,
-  });
+  try {
+    await page.waitForSelector(".user-image", {
+      timeout: 5000,
+    });
+  } catch (err) {
+    await handle2StepAuth(page);
+    await page.waitForSelector(".user-image", {
+      timeout: 5000,
+    });
+  }
 
   const cookies = await page.cookies("https://divisist2.ufps.edu.co/");
 
@@ -55,4 +68,40 @@ export async function getCookie() {
   await browser.close();
 
   return sessionCookie.value;
+}
+
+async function forceClick(page: Page, selector: string, timeout = 5000) {
+  await page.waitForFunction(
+    (selector) => {
+      const elem = document.querySelector(selector) as HTMLElement;
+      const exists = elem !== null;
+
+      if (exists) {
+        elem.click();
+      }
+
+      return exists;
+    },
+    {
+      timeout,
+    },
+    selector,
+  );
+}
+
+async function handle2StepAuth(page: Page) {
+  try {
+    // If google directly tries to use notification login
+    await forceClick(page, "main div:nth-of-type(2) button", 2000);
+  } catch (err) {}
+
+  await forceClick(page, "[data-challengeid='4']");
+
+  const tokenInput = await page.waitForSelector("#totpPin", {
+    visible: true,
+  });
+
+  await tokenInput?.type(authenticator.generate(GOOGLE_TOTP_SECRET));
+
+  await forceClick(page, "#totpNext");
 }
