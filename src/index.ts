e7699d5c294b032f1644e5a4086af5f3cc20a6e1
Juan Afanador <juanafanador07@gmail.com>
2025-11-02 19:34:49 -0500

    feat: Add logs

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,15 @@ const app = express();
 app.get("/cookie", async (req, res) => {
   try {
     const cookie = await getCookie();
+
+    console.log("Cookie obtained sucessfully");
+
     res.send({
       cookie,
     });
   } catch (err) {
+    console.log(err);
+
     if (err instanceof Error) {
       res.send({
         error: true,
